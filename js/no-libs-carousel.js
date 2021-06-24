dd9ceec0a709b68ed9aaa858bd0296c41e644b86
NoraLou <nora.humpage@gmail.com>
2021-06-23 23:21:58 -0400

    correct next and prev css positions then get logic moving next working

--- js/no-libs-carousel.js
+++ js/no-libs-carousel.js
@@ -6,10 +6,7 @@ class Carousel {
   constructor() {
     this.imagesArr = document.getElementsByClassName(BASE_CLASS_NAME);
     this.moving = true;
-
     this.curr = 0;
-    this.prev = this.imagesArr.length - 1;
-    this.next = this.curr++;
 
     this.moveNext = this.moveNext.bind(this)
     this.movePrev = this.movePrev.bind(this)
@@ -18,55 +15,70 @@ class Carousel {
   init() {
     console.log('Init!')
     this.imagesArr[this.imagesArr.length - 1].classList.add('prev');
-    this.imagesArr[0].classList.add('active');
+    this.imagesArr[this.curr].classList.add('active');
     this.imagesArr[1].classList.add('next')
     this.moving = false;
   }
 
-  // disableInteraction() {
-  //   this.moving = true;
-  //   setTimeout(function () {
-  //     this.moving = false
-  //   },500)
-  // }
-
   loop() {
   }
 
-  increment() {
-    // console.log('increment!')
-    let oldPrev = this.prev;
-    this.prev = this.curr;
-    this.curr = this.next;
-    if (this.next === this.imagesArr.length - 1) {
-      this.next = 0
-    } else {
-      this.next++
+  goToSlide(slide) {
+    let newPrev = slide - 1;
+    let newNext = slide + 1;
+    let oldPrev = slide - 2;
+    let oldNext = slide + 2;
+
+    //check if the newPrev if out of bounds
+    if (newPrev <= 0) {
+      oldPrev = (this.imagesArr.length - 1)
+    } else if (newNext >= (this.imagesArr.length - 1)) {
+      oldNext = 0
     }
-    this.imagesArr[oldPrev].classList = BASE_CLASS_NAME
-    this.imagesArr[this.prev].classList = BASE_CLASS_NAME + ' prev'
-    this.imagesArr[this.curr].classList = BASE_CLASS_NAME + ' active'
-    this.imagesArr[this.next].classList = BASE_CLASS_NAME + ' next'
+
+    //check if currSlide is at beginning or end
+    if (slide === 0) {
+      newPrev = (this.imagesArr.length - 1);
+      oldPrev = (this.imagesArr.length - 2);
+      oldNext = (slide + 1);
+    } else if (slide === (this.imagesArr.length - 1)) {
+      newPrev = (slide - 1);
+      newNext = 0;
+      oldNext = 1;
+    }
+
+    this.imagesArr[oldPrev].className = BASE_CLASS_NAME;
+    this.imagesArr[oldNext].className = BASE_CLASS_NAME;
+    this.imagesArr[newPrev].className = BASE_CLASS_NAME + ' prev';
+    this.imagesArr[slide].className = BASE_CLASS_NAME + ' active';
+    this.imagesArr[newNext].className = BASE_CLASS_NAME + ' next';
   }
 
+
   moveNext() {
-    this.increment()
+    console.log('move next')
+    if (this.curr === this.imagesArr.length - 1) {
+      this.curr = 0;
+    } else {
+      this.curr++
+    }
+    //move to updated value
+    this.goToSlide(this.curr)
   }
 
   movePrev() {
-    console.log('move prev!')
 
   }
 
 }
 
-const prevBtn = document.querySelector('.carousel__btn--next')
-const nextBtn = document.querySelector('.carousel__btn--prev')
+const nextBtn = document.querySelector('.carousel__btn--next')
+const prevBtn = document.querySelector('.carousel__btn--prev')
 const myCarousel = new Carousel();
 
-console.log('prevButton', prevBtn)
-console.log('nextButton', prevBtn)
-console.log(`imagesArr ${myCarousel.imagesArr}`)
+// console.log('prevButton', prevBtn)
+// console.log('nextButton', prevBtn)
+// console.log(`imagesArr ${myCarousel.imagesArr}`)
 
 window.onload = myCarousel.init();
 prevBtn.addEventListener('click',  myCarousel.movePrev);
